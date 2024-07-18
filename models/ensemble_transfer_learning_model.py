3c563ce26a61b1bdab6d7da09546a816d010843d
KOSASIH <kosasihg88@gmail.com>
2024-07-18 22:26:31 +0700

    Create ensemble_transfer_learning_model.py

---
+++ models/ensemble_transfer_learning_model.py
@@ -0,0 +1,26 @@
+import numpy as np
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense
+
+class EnsembleTransferLearningModel:
+    def __init__(self):
+        self.model = self.create_model()
+
+    def create_model(self):
+        # Create ensemble transfer learning model using pre-trained weights
+        input_layer = Input(shape=(10,))
+        x = Dense(64, activation='relu')(input_layer)
+        x = Dense(32, activation='relu')(x)
+        output_layer = Dense(1, activation='linear')(x)
+        model = Model(inputs=input_layer, outputs=output_layer)
+        model.load_weights('pre_trained_weights.h5')
+        return model
+
+    def fine_tune(self, X, y):
+        # Fine-tune ensemble transfer learning model
+        self.model.fit(X, y, epochs=100, batch_size=32, verbose=2)
+
+    def predict(self, X):
+        # Make predictions using ensemble transfer learning model
+        predictions = self.model.predict(X)
+        return predictions
