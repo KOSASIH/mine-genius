debc9b08b3b9c5df96e848420e7ad2d5225cbea8
KOSASIH <kosasihg88@gmail.com>
2024-07-18 22:10:43 +0700

    Create ensemble_model.py

---
+++ models/ensemble_model.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+class EnsembleModel:
+    def __init__(self):
+        self.model = RandomForestRegressor(n_estimators=100)
+
+    def train(self, X, y):
+        # Train ensemble model
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X):
+        # Make predictions using ensemble model
+        predictions = self.model.predict(X)
+        return predictions
