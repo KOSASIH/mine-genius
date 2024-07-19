641bbc419a2e4fb1455c1ea3be2c25a3e209e4ed
KOSASIH <kosasihg88@gmail.com>
2024-07-19 09:55:32 +0700

    Create physics_engine.py

---
+++ simulator/physics_engine.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class PhysicsEngine:
+    def __init__(self):
+        self.gravity = 9.81
+
+    def simulate_collision(self, object1, object2):
+        # simulates a collision between two objects
+        pass
+
+    def simulate_motion(self, object, time_step):
+        # simulates the motion of an object over a given time step
+        pass
+
+    def simulate_gravity(self, object, time_step):
+        # simulates the effect of gravity on an object over a given time step
+        pass
