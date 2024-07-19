0b9d9bf9bb04a45bc70f13cf3043956477fe64a7
KOSASIH <kosasihg88@gmail.com>
2024-07-19 09:41:22 +0700

    Create algorithms.py

---
+++ mine-genius/features/algorithms.py
@@ -0,0 +1,19 @@
+def binary_search(arr, target):
+    # implements binary search algorithm
+    pass
+
+def merge_sort(arr):
+    # implements merge sort algorithm
+    pass
+
+def quick_sort(arr):
+    # implements quick sort algorithm
+    pass
+
+def breadth_first_search(graph, start):
+    # implements breadth-first search algorithm
+    pass
+
+def depth_first_search(graph, start):
+    # implements depth-first search algorithm
+    pass
